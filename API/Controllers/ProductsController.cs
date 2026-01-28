4ed372395e3e1425d5f34dba9978dba064dfb8de
Sambidhan26 <sambidhan26@gmail.com>
2026-01-28 09:43:16 +0545

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,86 @@
+﻿using Core.Entities;
+using Infrastrucutre.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product; //CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(u => u.Id == id);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Product>> UpdateProduct(int id, Product product)
+        {
+            if (product.Id != id || !ProductExists(id))
+            {
+                return BadRequest("Cannot update this Product");
+            }
+            else
+            { 
+
+                _context.Entry(product).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                
+                return NoContent();
+            }
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null )
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+            return NoContent();
+        }
+
+
+    }
+}
