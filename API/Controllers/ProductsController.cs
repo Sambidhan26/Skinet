94fc7e55d6c4b810db3ec8e7c84d6c3522d0dbc3
Sambidhan26 <sambidhan26@gmail.com>
2026-02-10 14:45:06 +0545

    Generic Repository implemetation for mutiple Entities, Brand Specification and Type Specification worked around all type of Entities.

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Entities;
 using Core.Interfaces;
+using Core.Specification;
 using Infrastrucutre.Data;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -9,20 +10,22 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController(IProductRepository repo) : ControllerBase
+    public class ProductsController(IGenericRepository<Product> repo) : ControllerBase
     {
 
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
         {
-            var products = await repo.GetProductsAsync(brand, type, sort);
+            var spec = new ProductSpecification(brand, type, sort);
+            var products = await repo.ListAsync(spec);
+
             return Ok(products);
         }
 
         [HttpGet("{id:int}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await repo.GetProductByIdAsync(id);
+            var product = await repo.GetByIdAsync(id);
             if (product == null)
             {
 
@@ -34,7 +37,7 @@ namespace API.Controllers
         [HttpPost]
         public async Task<ActionResult<Product>> CreateProduct(Product product)
         {
-            repo.AddProduct(product);
+            repo.Add(product);
 
             if( await repo.SaveAllAsync())
             {
@@ -50,7 +53,7 @@ namespace API.Controllers
             {
                 return BadRequest("Cannot update this Product");
             }
-                repo.UpdateProduct(product);
+                repo.Update(product);
                 if (await repo.SaveAllAsync())
                 {
                     return  NoContent();
@@ -62,13 +65,13 @@ namespace API.Controllers
         [HttpDelete("{id:int}")]
         public async Task<ActionResult<Product>> DeleteProduct(int id)
         {
-            var product = await repo.GetProductByIdAsync(id);
+            var product = await repo.GetByIdAsync(id);
 
             if (product == null )
             {
                 return NotFound();
             }
-                repo.DeleteProduct(product);
+                repo.Remove(product);
             if (await repo.SaveAllAsync())
             {
                 return NoContent();
@@ -79,22 +82,22 @@ namespace API.Controllers
         }
 
         [HttpGet("brands")]
-        public async Task<ActionResult<IReadOnlyList<string>>> GetProductBrands()
+        public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
         {
-            var brands = await repo.GetBrandAsync();
-            return Ok(brands);
+            var spec = new BrandListSpecification();
+            return Ok(await repo.ListAsync(spec));
         }
 
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
-        {   
-            var types = await repo.GetTypesAsync();
-            return Ok(types);
+        {
+            var spec = new TypeListSpecification();
+            return Ok(await repo.ListAsync(spec));
         }
 
         private bool ProductExists(int id)
         {
-            return repo.ProductExists(id);
+            return repo.Exists(id);
         }
 
     }
---
+++ Core/Specification/ProductSpecification.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specification
+{
+    public class ProductSpecification: BaseSpecification<Product>
+    {
+        public ProductSpecification(string? brand, string? type, string? sort): base(u => 
+        (string.IsNullOrWhiteSpace(brand) || u.Brand == brand) && 
+        (string.IsNullOrWhiteSpace(type) || u.Type == type)
+        )
+        {
+            switch(sort)
+            {
+                case "priceAsc":
+                    AddOrderBy(u => u.Price);
+                    break;
+                case "priceDesc":
+                    AddOrderByDescending(u => u.Price);
+                    break;
+                default:
+                    AddOrderBy(u => u.Name);
+                    break;
+            }
+        }
+    }
+}
